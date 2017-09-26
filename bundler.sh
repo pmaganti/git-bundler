a493d4d84d0c5940dd7dcff52d70cc8a3f599a68
Pavan Maganti <pmaganti@rapidbizapps.com>
2017-09-26 14:13:28 -0700

    Create bundler.sh

---
+++ bundler.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#Reading the git remote repo url
+echo -n 'Remote repo url: '
+read gitrepo
+
+#Cloning the git remote repo to a folder
+echo -n 'Project folder: '
+read projectfolder
+
+mkdir $projectfolder && cd $projectfolder && git clone https://github.com/rapidbizapps/$gitrepo . &&
+for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
+git branch --track ${branch#remotes/origin/} $branch || git branch
+done && git bundle create ../bundles/$projectfolder.bundle --all && git bundle verify ../bundles/$projectfolder.bundle && echo "Done $projectfolder"
